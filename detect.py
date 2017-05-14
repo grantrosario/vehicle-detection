1f85d9a5977b62cebed6850d0e3567966157ec51
Grant Rosario <grantster1124@aol.com>
2017-05-13 20:07:43 -0400

    Added sliding windows

--- detect.py
+++ detect.py
@@ -23,6 +23,8 @@ ORIENT = 9
 PIX_PER_CELL = 8
 CELL_PER_BLOCK = 2
 HOG_CHANNEL = 0
+WIND_SIZE = 64
+WIND_OVERLAP = 0.5
 
 #---------------------
 
@@ -86,6 +88,53 @@ def extract_features(imgs, color_space = COLOR_SPACE, orient = ORIENT, pix_per_c
         features.append(np.concatenate((spatial_features, color_features, hog_features)))
     return features
 
+def slide_window(img, x_start_stop = [None, None], y_start_stop = [None, None], xy_window=(WIND_SIZE, WIND_SIZE), xy_overlap = (WIND_OVERLAP, WIND_OVERLAP)):
+    if x_start_stop[0] == None:
+        x_start_stop[0] = 0
+    if x_start_stop[1] == None:
+        x_start_stop[1] = img.shape[1]
+    if y_start_stop[0] == None:
+        y_start_stop[0] = 0
+    if y_start_stop[1] == None:
+        y_start_stop[1] = img.shape[0]
+
+    # Region to be searched
+    xspan = x_start_stop[1] - x_start_stop[0]
+    yspan = y_start_stop[1] - y_start_stop[0]
+
+    # Compute the number of pixels per step in x/y
+    nx_pix_per_step = np.int(xy_window[0]*(1 - xy_overlap[0]))
+    ny_pix_per_step = np.int(xy_window[1]*(1 - xy_overlap[1]))
+
+    # Compute the number of windows in x/y
+    nx_buffer = np.int(xy_window[0]*(xy_overlap[0]))
+    ny_buffer = np.int(xy_window[1]*(xy_overlap[1]))
+    nx_windows = np.int((xspan - nx_buffer)/nx_pix_per_step)
+    ny_windows = np.int((yspan - ny_buffer)/ny_pix_per_step)
+
+    window_list = []
+    for ys in range(ny_windows):
+        for xs in range(nx_windows):
+            startx = xs*nx_pix_per_step + x_start_stop[0]
+            endx = startx + xy_window[0]
+            starty = ys*ny_pix_per_step + y_start_stop[0]
+            endy = starty + xy_window[1]
+            window_list.append(((startx, starty), (endx, endy)))
+    return window_list
+
+def draw_boxes(img, bboxes, color = (0, 0, 255), thick = 6):
+    imcopy = np.copy(img)
+    for bbox in bboxes:
+        cv2.rectangle(imcopy, bbox[0], bbox[1], color, thick)
+    return imcopy
+
+image = mpimg.imread('test_images/test1.jpg')
+windows = slide_window(image, x_start_stop = [None, None], y_start_stop = [350, None],
+                       xy_window = (WIND_SIZE, WIND_SIZE), xy_overlap = (WIND_OVERLAP, WIND_OVERLAP))
+
+window_img = draw_boxes(image, windows, color = (255, 0, 0), thick = 3)
+plt.imshow(window_img)
+plt.show()
 
 car_features = extract_features(cars, COLOR_SPACE, ORIENT, PIX_PER_CELL, CELL_PER_BLOCK, HOG_CHANNEL)
 notcar_features = extract_features(notcars, COLOR_SPACE, ORIENT, PIX_PER_CELL, CELL_PER_BLOCK, HOG_CHANNEL)
